6a637c0f3f5e91666c924a6d12aea7e33baee69f
Stephan Loh <sloh4137@gmail.com>
2020-03-06 16:47:43 -0500

    Add about us and food sections

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,6 +7,11 @@ import IndexNavbar from "components/Navbars/IndexNavbar.js";
 import IndexHeader from "components/Headers/IndexHeader.js";
 import SectionAboutUs from "components/Sections/SectionAboutUs";
 import SectionCarousels from "components/Sections/SectionCarousels";
+import Footer from "components/Footers/Footer";
+
+import {
+    Button,
+  } from "reactstrap";
 
 const HomePage = (props) => {
 
@@ -17,7 +22,66 @@ const HomePage = (props) => {
             <div className="main">
                 <SectionAboutUs />
                 <SectionCarousels />
+                <div className="section" >
+                <Button
+                  className="btn-round mr-1"
+                  color="default"
+                  outline
+                  type="button"
+                >
+                  Default
+                </Button>
+                <Button
+                  className="btn-round mr-1"
+                  color="primary"
+                  outline
+                  type="button"
+                >
+                  Primary
+                </Button>
+                <Button
+                  className="btn-round mr-1"
+                  color="info"
+                  outline
+                  type="button"
+                >
+                  Info
+                </Button>
+                <Button
+                  className="btn-round mr-1"
+                  color="success"
+                  outline
+                  type="button"
+                >
+                  Success
+                </Button>
+                <Button
+                  className="btn-round mr-1"
+                  color="warning"
+                  outline
+                  type="button"
+                >
+                  Warning
+                </Button>
+                <Button
+                  className="btn-round mr-1"
+                  color="danger"
+                  outline
+                  type="button"
+                >
+                  Danger
+                </Button>
+                <Button
+                  className="btn-round"
+                  outline
+                  color="neutral"
+                  type="button"
+                >
+                  Neutral
+                </Button>
+                </div>
             </div>
+            <Footer />
         </>
     );
 };

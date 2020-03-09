cde2c7afeafd561bb29be10e412d4d07a311f524
Stephan Loh <sloh4137@gmail.com>
2020-03-08 23:53:18 -0400

    Update formatting

--- src/components/Sections/SectionAboutUs.jsx
+++ src/components/Sections/SectionAboutUs.jsx
@@ -29,11 +29,7 @@ const SectionAboutUs = (props) => {
 
   return (
     <>
-      {/*  <div className="section section-image text-center" id="about-us"
-      style={{
-         backgroundImage: "url(" + require("assets/img/background.jpg") + ")"
-       }}> */}
-      <div className="section section-dark text-center" id="about-us">
+      <div className="section section-dark section-about-us" id="about-us">
         <div className="lantern-banner">
           <img alt="lantern banner" src={require("assets/img/lantern-banner.png")} />
         </div>
@@ -43,28 +39,40 @@ const SectionAboutUs = (props) => {
         <Container>
           <Row>
             <Col className="ml-auto mr-auto" md="8" sm="12">
-              <h2 className="title">What is a Night Market?</h2>
-              <h5 className="description">
+              <h2 className="title text-left">What is a Night Market?</h2>
+
+              <h5 className="description text-left">
                 An amazing Night Market description that lasts for many paragraphs.
                 It is so amazing that wow I can't even type it all out. So cool.
                 Such grace. Truly tremendous. Something about 小吃. Something about 夜市. Nice.
                 Thank you for reading this wonderful content
             </h5>
+
+              <h3 className="description">
+                <FontAwesomeIcon className="icon icon-success" icon={faMapMarkerAlt} /> Jiufen
+              </h3>
+
+              <h5 className="description text-left">
+                Jiufen is a cool city. More info about Jiufen. A link to more information about Jiufen
+              </h5>
               <br />
 
-              <h5 className="description">
-                <FontAwesomeIcon className="icon" icon={faMapMarkerAlt} /> Jiufen
-            </h5>
-
-              <br />
-              <Button
-                className="btn-round"
-                color="info"
-                href=""
-                onClick={e => e.preventDefault()}
-              >
-                See Details
+              <Row>
+                <Col className="ml-auto mr-auto" sm="6">
+                  <Button
+                    className="btn-round"
+                    color="info"
+                    href=""
+                    block
+                    onClick={e => e.preventDefault()}
+                  >
+                    See Details
                 </Button>
+                </Col>
+              </Row>
+
+
+
 
             </Col>
             <Col className="ml-auto mr-auto" md="4" sm="12">

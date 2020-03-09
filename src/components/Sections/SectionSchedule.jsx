e1a83037e8d1611c4757ca05d059db725eed1502
Stephan Loh <sloh4137@gmail.com>
2020-03-08 23:52:58 -0400

    Add schedule section

--- src/components/Sections/SectionSchedule.jsx
+++ src/components/Sections/SectionSchedule.jsx
@@ -3,11 +3,60 @@
  */
 
 import React from "react";
+import { useStaticQuery, graphql } from "gatsby";
+import { Container, Col, Row, Table } from "reactstrap";
 
 const SectionSchedule = (props) => {
+    const data = useStaticQuery(graphql`
+    query {
+        allGoogleSheetWebsiteScheduleRow {
+          nodes {
+            event
+            time
+            id
+          }
+        }
+      }      
+    `);
+
     return (
-        <>
-        </>
+        <div className="section section-dark" id="schedule">
+            <Container>
+                <Row>
+                    <Col sm="12">
+                        <h1 className="title text-center">Schedule</h1>
+                    </Col>
+                </Row>
+
+                <Row>
+                    <Col className="ml-auto mr-auto" sm="8">
+                        <Table striped>
+                            <thead>
+                                <tr>
+                                    <th className="info-border-color">
+                                        Time
+                                    </th>
+                                    <th className="info-border-color">
+                                        Event
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {data.allGoogleSheetWebsiteScheduleRow.nodes.map(row => {
+                                    return (
+                                        <tr>
+                                            <td>{row.time}</td>
+                                            <td>{row.event}</td>
+                                        </tr>
+                                    );
+                                })}
+                            </tbody>
+                        </Table>
+                    </Col>
+                </Row>
+
+            </Container>
+        </div>
     );
 }
 
